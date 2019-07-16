030d190ddf91b9c856210601ba3ae717d9c2adc9
Himanshu Walia <waliahimanshu@Himanshus-MacBook-Pro.local>
2019-07-16 22:24:18 +0100

    add doc comments

--- APICall.js
+++ APICall.js
@@ -8,42 +8,43 @@ var options = {
     method: 'GET'
 };
 
-let responseData = [];
+// DTO (Data transfer object)
+let responseDto = [];
 
-// http call to giphy endoint 
+// http call to giphy endpoint 
 http.get(options, (resp) => {
     console.log('STATUS: ' + resp.statusCode);
     console.log('statusMessage: ' + resp.statusMessage);
     // A chunk of data has been received.
     resp.on('data', (chunk) => {
-        responseData += chunk;
+        responseDto += chunk;
     });
 
     // The whole response has been received. Get the result.
     resp.on('end', () => {
-        setData(responseData);
+        setData(responseDto);
     });
 
 }).on("error", (err) => {
     console.log("Error: " + err.message);
 });
 
-function setData(responseData) {
-    // parse raw data into js object
+function setData(responseDto) {
+    // parse raw data into js object aka DTO object to Domain object
     var object = JSON.parse(responseData);
+    var giphyDomainArr = [];
 
-    var arr = [];
     for (let i = 0; i < object.data.length; i++) {
-        arr.push(new GiphyDomain(object.data[i].url, object.data[i].title));
+        giphyDomainArr.push(new GiphyDomain(object.data[i].url, object.data[i].title));
     }
 }
 
 /**
- * Domain Object
+ * Domain Object Representation
  */
 class GiphyDomain {
-    constructor(url, title) {
-        this.url = url; // class varaibles
+    constructor(url, title) { // class constructor with two params
+        this.url = url; // class variables
         this.title = title; // class variables
     }
 }
